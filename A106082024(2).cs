bbad122c8b2ab4ed3693951e5f4b1ffef8e16322
3/29 <sadqwd>
2018-03-29 18:07:13 +0800

    身高

---
+++ A106082024(2).cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0 ;
+            double pppp = 0; 
+
+
+            string[] sex = { "男", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "男", "女", "男", "男", "男", "女", "女", "男", "男", "女", "男", "女", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "女", "男", "男", "女", "女", "男", "男", "女", "女", "男", "男", "男", "男", "男", "男", "女", "男", "女", "女", "女", "女", "女", "女", "男", "女", "女", "女", "女", "女", "男", "男", "女", "女", "女", "男" };
+
+            int[] hei = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 167, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 165, 155, 0, 168, 160, 169, 0, 150, 163, 168, 173 };
+
+            for (int i = 0; i < hei.Length; i++) 
+            {
+                if (hei[i] > 100 && hei[i] < 200)
+                {
+                    if(sex[i] == "女")
+                    {
+                        sum = sum + hei.Length;
+                        pppp++; 
+
+                    }
+                }
+
+                
+
+            }
+
+            double percent = (double)sum / (double)pppp; 
+
+                
+            Console.WriteLine("總身高 : " + sum);
+            Console.WriteLine("人數 : " + pppp);
+            Console.WriteLine("平均身高 : " + percent); 
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
